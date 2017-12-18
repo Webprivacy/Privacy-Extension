b554d126bd6e61ba709f8fe259f866e4e88410ab
h33raj <heeraj123@gmail.com>
2017-12-18 09:29:34 +0530

    Fixing the bug

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+// Intialize the num_tabs with tab length
+chrome.tabs.getAllInWindow( null, function( tabs ){
+    console.log("Initial tab count: " + tabs.length);
+    num_tabs = tabs.length;
+});
+
+// Update the num_tabs when tab is created
+chrome.tabs.onCreated.addListener(function(tab){
+    num_tabs++;
+    console.log("Tab created event caught. Open tabs #: " + num_tabs);
+});
+
 // Block all cookies except session cookies 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contentSettings.cookies.set({
@@ -27,11 +39,21 @@ function erase(){
 
 // Detect browser window close event
 chrome.windows.onRemoved.addListener(function(windowid) {
-	alert("Thanks for Using Amrita Privacy badger!!");
-	erase();
+  // We may need it in future
 })
 
 // Detect the tab closing event
 chrome.tabs.onRemoved.addListener(function(tabid, removed) {
-	//We will need you in future
+	// Update the num_tabs
+  num_tabs--;
+  console.log("Tab removed event caught. Open tabs #: " + num_tabs);
+  if( num_tabs == 0 ){
+    // Erase all the browser data when num_tabs is 0
+    erase();
+  }
 })
+
+// Chrome storage API
+chrome.storage.sync.get("data", function(items) {
+  
+});
--- popup.js
+++ popup.js
@@ -1,60 +1,17 @@
-function toggleImages(tab) {
-
-    chrome.contentSettings['images'].get({
-        primaryUrl: tab.url
-    }, function (details) {
-        chrome.contentSettings['cookies'].set({
-            primaryPattern: '<all_urls>',
-            setting: details.setting == 'block'
-        })
+var cookie, browserData;
+    
+$(document).ready(function() {
+    $("#cookie").click(function() {
+        chrome.storage.sync.set({'cookie': true}, function() {
+          // Notify that we saved.
+          //message('Settings saved');
+        });
     });
-
-}
-
-chrome.browserAction.onClicked.addListener(toggleImages);
-
-/*
-var incognito;
-var url;
-
-
-function settingChanged() {
-  var type = this.id;
-  var setting = this.value;
-  var pattern = /^file:/.test(url) ? url : url.replace(/\/[^\/]*?$/, '/*');
-  chrome.contentSettings[type].set({
-        'primaryPattern': pattern,
-        'setting': setting,
-        'scope': (incognito ? 'incognito_session_only' : 'regular')
-      });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var current = tabs[0];
-    incognito = current.incognito;
-    url = current.url;
-    var types = ['cookies', 'images', 'javascript', 'location', 'plugins',
-                 'popups', 'notifications', 'microphone', 'camera',
-                 'unsandboxedPlugins', 'automaticDownloads'];
-    types.forEach(function(type) {
-      // HACK: [type] is not recognised by the docserver's sample crawler, so
-      // mention an explicit
-      // type: chrome.contentSettings.cookies.get - See http://crbug.com/299634
-      chrome.contentSettings[type] && chrome.contentSettings[type].get({
-            'primaryUrl': url,
-            'incognito': incognito
-          },
-          function(details) {
-            document.getElementById(type).disabled = false;
-            document.getElementById(type).value = details.setting;
-          });
+    $("#browserdata").click(function() {
+        chrome.storage.sync.set({'browserdata': true}, function() {
+          // Notify that we saved.
+          //message('Settings saved');
+        });
     });
-  });
-
-  var selects = document.querySelectorAll('select');
-  for (var i = 0; i < selects.length; i++) {
-    selects[i].addEventListener('change', settingChanged);
-  }
 });
-*/
+
