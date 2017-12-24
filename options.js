a862b746445f90c3b180f327396c127754140322
h33raj <heeraj123@gmail.com>
2017-12-24 14:52:04 +0530

    Adding new feature (Spoofing height and width)

--- options.js
+++ options.js
@@ -72,3 +72,12 @@ window.onload = function() {
         console.log(node[i].src);
     }
 }
+
+// Code for spoofing height and width is disabled
+var s = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+s.src = chrome.extension.getURL('script.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
---
+++ script.js
@@ -0,0 +1,2 @@
+// Change the screen resolution
+screen = new function() { this.width = 1320; this.height = 738 }
