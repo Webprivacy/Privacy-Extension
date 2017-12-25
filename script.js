91191379a87a8744ee3a8ada373cd09361accc23
h33raj <heeraj123@gmail.com>
2017-12-25 18:25:05 +0530

    new changes

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 // Change the screen resolution
-screen = new function() { this.width = 1320; this.height = 738 }
+screen = new function() { this.width = 1320; this.height = 738; this.colorDepth = 700; this.availHeight=700}
