b4b950c7f6bec60fe65d5058f5b567d230db9c07
Gokul Krishna P <gkrishna@Gokuls-MacBook-Air.local>
2018-01-13 20:15:05 +0530

    Adding multiple UserAgents

--- background.js
+++ background.js
@@ -102,7 +102,13 @@ chrome.storage.sync.get("data", function (items) {
 
 });
 
-var userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
+var item = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
+                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+                 "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+                 "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)",
+                 "Googlebot/2.1 (+http://www.google.com/bot.html)"];
+
+var userAgent = item[Math.floor(Math.random()*item.length)];
 
 var requestFilter = {
 	urls: [
