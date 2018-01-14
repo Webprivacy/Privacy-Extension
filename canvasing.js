6ebbc3cf9a93df866c34cf7b6a32598fd387fab6
h33raj <heeraj123@gmail.com>
2018-01-14 22:13:08 +0530

    Add

--- canvasing.js
+++ canvasing.js
@@ -7,9 +7,9 @@ document.createElement = function(x) {
     ret_canvas.toDataURL = function() {
         var randomID = Math.random().toString(10);
         var saved_style = ret_canvas.getContext('2d').fillStyle;
-        var r = 255*Math.random()|0,
-            g = 255*Math.random()|0,
-            b = 255*Math.random()|0
+        var r = 0,
+            g = 0,
+            b = 0
             a = .01;
         ret_canvas.getContext('2d').fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
         ret_canvas.getContext('2d').fillRect(0,0,ret_canvas.width,ret_canvas.height);
@@ -28,13 +28,17 @@ document.createElement = function(x) {
             var imagedata = ret_ctx.orig_getimagedata(w,x,y,z);
             for (var i = 0; i < imagedata.data.length; i+=4) {
                 //alpha blend the pixel to emulate ctx fill
+                /*
                 var r = 255*Math.random()|0,
                     g = 255*Math.random()|0,
                     b = 255*Math.random()|0
+
                 imagedata[i] = r +  (imagedata[i] * .99);
                 imagedata[i+1] = g +  (imagedata[i+1] * .99);
                 imagedata[i+2] = b +  (imagedata[i+2] * .99);
                 imagedata[i+3] = a +  (imagedata[i+3] * .99);
+                */
+                imagedata[i] = 250;
             }
 
             return imagedata;
